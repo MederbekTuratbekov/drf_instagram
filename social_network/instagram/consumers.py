dee7833908ef573385402ed5d55240cf6b7a11f1
MederbekTuratbekov <142796921+MederbekTuratbekov@users.noreply.github.com>
2026-08-04 17:44:40 +0600

    ...

--- social_network/instagram/consumers.py
+++ social_network/instagram/consumers.py
@@ -1,6 +1,7 @@
-# chat/consumers.py
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from .models import Chat, Message
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -8,28 +9,30 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
 
-        # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave room group
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
-    # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
+        user = self.scope["user"]
 
-        # Send message to room group
+        await self.save_message(user, self.room_name, message)
+
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat.message", "message": message}
+            self.room_group_name,
+            {"type": "chat.message", "message": message, "author": user.username}
         )
 
-    # Receive message from room group
     async def chat_message(self, event):
         message = event["message"]
+        author = event.get("author")
+        await self.send(text_data=json.dumps({"message": message, "author": author}))
 
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message}))
+    @database_sync_to_async
+    def save_message(self, user, room_name, message):
+        chat, _ = Chat.objects.get_or_create(id=room_name)
+        Message.objects.create(author=user, text=message, chat=chat)
