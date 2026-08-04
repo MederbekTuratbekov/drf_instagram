6620bbc598522568de817e90adc7f7d1d6e01c4e
MederbekTuratbekov <142796921+MederbekTuratbekov@users.noreply.github.com>
2026-08-04 17:36:52 +0600

    ...

--- social_network/instagram/admin.py
+++ social_network/instagram/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from modeltranslation.admin import TranslationAdmin
 from .models import UserProfile, Follow, Post, PostLike, Comment, CommentLike, Story, Save, SaveItem
-from .translation import UserProfileTranslationOptions, PostTranslationOptions, CommentTranslationOptions
 
 
 admin.site.register(Follow)
@@ -11,14 +10,17 @@ admin.site.register(Story)
 admin.site.register(Save)
 admin.site.register(SaveItem)
 
-@admin.register(UserProfile, Post, Comment)
+
 class TranslateAdmin(TranslationAdmin):
     class Media:
         js = (
-            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
             'modeltranslation/js/tabbed_translation_fields.js',
         )
         css = {
             'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
         }
+
+
+admin.site.register(UserProfile, TranslateAdmin)
+admin.site.register(Post, TranslateAdmin)
+admin.site.register(Comment, TranslateAdmin)
